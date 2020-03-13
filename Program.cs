dff83bbc3c604cc8aace8395f167e13ffb7772b4
Dawffydd Riv <iqdavid.riv@gmail.com>
2020-03-12 22:26:01 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ejercicioMetodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo,vamos a realizar un pequeño ejercicio de métodos!");
+        }
+    }
+}
