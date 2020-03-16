35320501b105a638f2ac1e964d3fe4e5abe0683c
Dawffydd Riv <iqdavid.riv@gmail.com>
2020-03-15 21:43:29 -0600

    Se agrega el metodo AreaEsfera

--- Program.cs
+++ Program.cs
@@ -22,13 +22,14 @@ namespace ejercicioMetodos
             Console.WriteLine("El resultado de la multiplicación es " + MultiplicaNumeros(num1, num2));
             Console.WriteLine("El resultado de la división es " + DivideNumeros(num1, num2));
             //Console.WriteLine("La suma  de los numeros es " + SumaNumeros(7,9));
+            Console.WriteLine("El área de la esfera es " + AreaEsfera(2));
         }
         static void mensajeEnpantalla()
         {
-            Console.WriteLine("Saludos desde el método mensajeEnpantalla en el cual solo esta escrita esta linea");
+            Console.WriteLine("Saludos desde el método mensajeEnpantalla, en el cual solo esta escrita esta linea");
         }
         static double SumaNumeros(double num1, double num2)
-        {
+        {   
             double resultado = num1 + num2;
             return resultado;
         }
@@ -46,6 +47,12 @@ namespace ejercicioMetodos
         {
             double resultado = num1 / num2;
             return resultado;
+        }
+        static double AreaEsfera(double radio)
+        {
+            
+            double Areae = 4 * Math.PI * radio;
+            return Areae;
         }
     }
 }
