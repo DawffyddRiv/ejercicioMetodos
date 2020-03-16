48879666eb44d38b118007882fc3c8afa662f003
Dawffydd Riv <iqdavid.riv@gmail.com>
2020-03-15 20:44:26 -0600

    Ejercicio de métodos con operaciones aritméticas

--- Program.cs
+++ Program.cs
@@ -6,28 +6,46 @@ namespace ejercicioMetodos
     {
         static void segundoMensaje()
         {
-            Console.WriteLine("Texto del método segundoMensaje");
+            Console.WriteLine("Saludos desde el método segundoMensaje");
         }
         static void Main(string[] args)
         {
-            //Se tiene que tomar en cuenta que el método main es el método de entrada y por el cual entran los demás
-            mensajeEnpantalla();
-            Console.WriteLine("Hola mundo,vamos a realizar un pequeño ejercicio de métodos!");
-            Console.WriteLine("Mensaje desde el metodo Main");
-            mensajeEnpantalla();
-            segundoMensaje();//invocando el método segundoMensaje
-            sumaNumeros(4,8);//Al invocar el método sumaNumeros es obligatorio introducir los parámetros y del tipo que pida el método
+            segundoMensaje();
+            Console.WriteLine("Introduce el primer numero");
+            double num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Introduce el segundo numero");
+            double num2 = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("El resultado de la suma es " + SumaNumeros(num1, num2));
+            Console.WriteLine("El resultado de la resta es " + RestaNumeros(num1, num2));
+            Console.WriteLine("El resultado de la multiplicación es " + MultiplicaNumeros(num1, num2));
+            Console.WriteLine("El resultado de la división es " + DivideNumeros(num1, num2));
+            //Console.WriteLine("La suma  de los numeros es " + SumaNumeros(7,9));
         }
         static void mensajeEnpantalla()
         {
-            Console.WriteLine("Esto es una linea para un mensaje en pantalla del metodo mensajeEnpantalla");
+            Console.WriteLine("Saludos desde el método mensajeEnpantalla en el cual solo esta escrita esta linea");
         }
-        static void sumaNumeros(int num1, int num2)
+        static double SumaNumeros(double num1, double num2)
         {
-
-            //Console.WriteLine("La suma de los numeros" + num1+num2); esta linea solo muestra los parametros (numeros introducidos) uno junto del otro
-            Console.WriteLine($"La suma de los números es: {num1+num2}");
-            Console.WriteLine("Tambien se puede sumar así:" + (num1+num2));
+            double resultado = num1 + num2;
+            return resultado;
+        }
+        static double RestaNumeros(double num1, double num2)
+        {
+            double resultado = num1 - num2;
+            return resultado;
+        }
+        static double MultiplicaNumeros(double num1, double num2)
+        {
+            double resultado = num1 * num2;
+            return resultado;
+        }
+        static double DivideNumeros(double num1, double num2)
+        {
+            double resultado = num1 / num2;
+            return resultado;
         }
     }
 }
